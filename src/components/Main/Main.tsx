15d247cb0d52d9c091fa040fe1d9d45e9e506050
Armand Philippot <git@armandphilippot.com>
2021-12-14 00:30:54 +0100

    chore: define main grid to center content

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,7 +1,12 @@
 import { FunctionComponent } from 'react';
+import styles from './Main.module.scss';
 
 const Main: FunctionComponent = ({ children }) => {
-  return <main>{children}</main>;
+  return (
+    <main className={styles.wrapper}>
+      <div className={styles.body}>{children}</div>
+    </main>
+  );
 };
 
 export default Main;
