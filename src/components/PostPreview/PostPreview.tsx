414474f61fe78a937cf570945db699bb1a915971
Armand Philippot <git@armandphilippot.com>
2022-01-08 17:45:16 +0100

    chore: adjust h2 styles

--- src/components/PostPreview/PostPreview.tsx
+++ src/components/PostPreview/PostPreview.tsx
@@ -37,7 +37,7 @@ const PostPreview = ({
         </div>
       )}
       <header className={styles.header}>
-        <TitleTag>
+        <TitleTag className={styles.title}>
           <Link href={`/article/${post.slug}`}>
             <a>{post.title}</a>
           </Link>
