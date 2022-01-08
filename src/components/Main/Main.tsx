4e878aab112b08a18f7285bbb0df1f20d38ee9cf
Armand Philippot <git@armandphilippot.com>
2022-01-08 18:42:06 +0100

    chore: ensure 100vh height and improve Header/Footer styles

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,7 +1,12 @@
 import { FunctionComponent } from 'react';
+import styles from './Main.module.scss';
 
 const Main: FunctionComponent = ({ children }) => {
-  return <main id="main">{children}</main>;
+  return (
+    <main id="main" className={styles.wrapper}>
+      {children}
+    </main>
+  );
 };
 
 export default Main;
