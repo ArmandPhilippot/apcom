d47b859b218679cc99fa95e64b2308c378dc93fe
Armand Philippot <git@armandphilippot.com>
2022-01-14 12:55:05 +0100

    chore: improve Branding component

--- src/components/Branding/Branding.tsx
+++ src/components/Branding/Branding.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import { ReactElement } from 'react';
 import { t } from '@lingui/macro';
 import photo from '@assets/images/armand-philippot.jpg';
+import Logo from '@assets/images/armand-philippot-logo.svg';
 import { config } from '@config/website';
 import styles from './Branding.module.scss';
 
@@ -14,18 +15,23 @@ const Branding: BrandingReturn = ({ isHome = false }) => {
   return (
     <div className={styles.wrapper}>
       <div className={styles.logo}>
-        <Image
-          src={photo}
-          alt={t({
-            message: `${config.name} picture`,
-            comment: 'Branding logo.',
-          })}
-          layout="intrinsic"
-        />
+        <div className={styles.logo__front}>
+          <Image
+            src={photo}
+            alt={t({
+              message: `${config.name} picture`,
+              comment: 'Branding logo.',
+            })}
+            layout="intrinsic"
+          />
+        </div>
+        <div className={styles.logo__back}>
+          <Logo />
+        </div>
       </div>
       <TitleTag className={styles.name}>
         <Link href="/">
-          <a>{config.name}</a>
+          <a className={styles.link}>{config.name}</a>
         </Link>
       </TitleTag>
       <p className={styles.job}>{config.baseline}</p>
