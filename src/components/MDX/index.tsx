5c3ea8b577e934237a5d03c477361763b167f57b
Armand Philippot <git@armandphilippot.com>
2022-01-26 15:16:59 +0100

    chore: add a Gallery component for MDX rendering

--- src/components/MDX/index.tsx
+++ src/components/MDX/index.tsx
@@ -1,5 +1,6 @@
 import CodeBlock from './CodeBlock/CodeBlock';
+import Gallery from './Gallery/Gallery';
 import Link from './Link/Link';
 import ResponsiveImage from './ResponsiveImage/ResponsiveImage';
 
-export { CodeBlock, Link, ResponsiveImage };
+export { CodeBlock, Gallery, Link, ResponsiveImage };
