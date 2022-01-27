7bb01fedbe68f9a71dd6c29174080a71b4e843f0
Armand Philippot <git@armandphilippot.com>
2022-01-27 16:50:41 +0100

    chore: update titles and meta desc

--- src/config/seo.ts
+++ src/config/seo.ts
@@ -1,32 +1,33 @@
 import { t } from '@lingui/macro';
+import { config } from './website';
 
 export const seo = {
   homepage: {
-    title: t`Armand Philippot | Front-end developer`,
-    description: t`Armand Philippot is a front-end developer located in France. He codes, he writes and he plays. Discover is website.`,
+    title: t`${config.name} | Front-end developer: WordPress/React`,
+    description: t`${config.name} is a front-end developer located in France. He codes and he writes mostly about web development and open-source.`,
   },
   blog: {
-    title: t`Blog: development, open source | Armand Philippot`,
-    description: t`Discover Armand Philippot's writings. He talks about web development and open source mostly.`,
+    title: t`Blog: development, open source - ${config.name}`,
+    description: t`Discover ${config.name}'s writings. He talks about web development, Linux and open source mostly.`,
   },
   cv: {
-    title: t`CV Front-end developer | Armand Philippot`,
-    description: t`Discover the curriculum of Armand Philippot, front-end developer located in France: skills, experiences and training.`,
+    title: t`CV Front-end developer - ${config.name}`,
+    description: t`Discover the curriculum of ${config.name}, front-end developer located in France: skills, experiences and training.`,
   },
   contact: {
-    title: t`Contact form | Armand Philippot`,
-    description: t`Contact Armand Philippot through its website. All you need to do it's to fill the contact form.`,
+    title: t`Contact form - ${config.name}`,
+    description: t`Contact ${config.name} through its website. All you need to do it's to fill the contact form.`,
   },
   legalNotice: {
-    title: t`Legal notice | Armand Philippot`,
-    description: t`Discover the ArmandPhilippot.com website legal notice.`,
+    title: t`Legal notice - ${config.name}`,
+    description: t`Discover the legal notice of ${config.name}'s website.`,
   },
   error404: {
-    title: t`Error 404: Page not found | Armand Philippot`,
+    title: t`Error 404: Page not found - ${config.name}`,
     description: '',
   },
   projects: {
-    title: t`Projects | Armand Philippot`,
-    description: t`Discover Armand Philippot projects. Mostly related to web development and open source.`,
+    title: t`Projects: open-source makings - ${config.name}`,
+    description: t`Discover ${config.name} projects. Mostly related to web development and open source.`,
   },
 };
