67ce6cc19cbfdf9692dc785d2fc88ecee4ed500c
Armand Philippot <git@armandphilippot.com>
2022-01-28 17:38:39 +0100

    build: configure webpack to use react-intl without parser

--- next.config.js
+++ next.config.js
@@ -30,7 +30,12 @@ const nextConfig = {
       path.join(__dirname, 'node_modules'),
     ],
   },
-  webpack: (config) => {
+  webpack: (config, { dev }) => {
+    if (!dev) {
+      // https://formatjs.io/docs/guides/advanced-usage#react-intl-without-parser-40-smaller
+      config.resolve.alias['@formatjs/icu-messageformat-parser'] =
+        '@formatjs/icu-messageformat-parser/no-parser';
+    }
     config.module.rules.push(
       {
         test: /\.pdf/,
