93f0360fe064fbfd83dfbcc8f74075788e13dd5c
Armand Philippot <git@armandphilippot.com>
2022-02-12 14:01:37 +0100

    build: remove the no-parser config for formatjs
    I don't know if I misunderstand the purpose of no-parser but I can't
    get it to work with strings containing a variable or plurals. They are
    always fallback to the english message. So I decided to disable this
    config.

--- next.config.js
+++ next.config.js
@@ -31,11 +31,6 @@ const nextConfig = {
     ],
   },
   webpack: (config, { dev }) => {
-    if (!dev) {
-      // https://formatjs.io/docs/guides/advanced-usage#react-intl-without-parser-40-smaller
-      config.resolve.alias['@formatjs/icu-messageformat-parser'] =
-        '@formatjs/icu-messageformat-parser/no-parser';
-    }
     config.module.rules.push(
       {
         test: /\.pdf/,
