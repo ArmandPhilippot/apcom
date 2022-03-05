7f58c9217cdd9fe1ef3f301f2dc9c895382d763f
Armand Philippot <git@armandphilippot.com>
2022-03-05 19:11:58 +0100

    build(docker): add help option to print script usage

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -15,11 +15,40 @@ loadenvs() {
 error() {
   printf "Error: stack name not defined.\n"
   printf "Usage: sh deploy.sh your-stack-name [OPTIONS]\n"
+  printf "You can display more information about usage with -h or --help.\n"
   exit 1
+}
+
+help() {
+  cat <<EOF
+# USAGE
+sh deploy.sh [your-stack-name] [OPTIONS]
+
+# DESCRIPTION
+The version command allows Dotig to print its version and to check for a new release.
+
+# OPTIONS
+If [your-stack-name] is not provided:
+-h, --help          Print this help.
+
+If [your-stack-name] is provided:
+--orchestrator string    Orchestrator to use (swarm|kubernetes|all)
+--prune                  Prune services that are no longer referenced
+--resolve-image string   Query the registry to resolve image digest and
+                          supported platforms
+                          ("always"|"changed"|"never") (default "always")
+--with-registry-auth     Send registry authentication details to Swarm agents
+
+# BUGS
+If you find any bug, you can report it on https://github.com/ArmandPhilippot/apcom/issues.
+EOF
+
+  exit
 }
 
 deploy() {
   [ $# -eq 0 ] && error
+  [ "$1" = "-h" ] || [ "$1" = "--help" ] && help
 
   _stack_name="$1"
   shift
