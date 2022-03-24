641cfdb0b28ffa0e6cd5d2342bc218613ce4d051
Armand Philippot <git@armandphilippot.com>
2022-03-24 19:14:15 +0100

    test(jest): define a custom render function to set providers

---
+++ __tests__/utils/test-utils.tsx
@@ -0,0 +1,44 @@
+import { render, RenderOptions } from '@testing-library/react';
+import { ThemeProvider } from 'next-themes';
+import { FC } from 'react';
+import { IntlProvider } from 'react-intl';
+
+type ProvidersConfig = {
+  locale?: 'en' | 'fr';
+};
+
+type CustomRenderOptions = {
+  providers: ProvidersConfig;
+  testingLibrary: Omit<RenderOptions, 'wrapper'>;
+};
+
+/**
+ * Return a component wrapped with Intl and Theme Provider.
+ *
+ * @returns A component wrapped Intl and Theme providers.
+ */
+const AllTheProviders: FC<ProvidersConfig> = ({ children, locale = 'en' }) => {
+  return (
+    <IntlProvider locale={locale}>
+      <ThemeProvider>{children}</ThemeProvider>
+    </IntlProvider>
+  );
+};
+
+/**
+ * Render a component with all the providers.
+ *
+ * @param {JSX.Element} ui - A React component.
+ * @param {CustomRenderOptions} [options] - An object of render options and providers options.
+ * @returns A React component wrapped with all the providers.
+ */
+const customRender = (ui: JSX.Element, options?: CustomRenderOptions) => {
+  const Component = () => ui;
+  render(<Component />, {
+    wrapper: () => <AllTheProviders {...options?.providers} />,
+    ...options?.testingLibrary,
+  });
+};
+
+export * from '@testing-library/react';
+export { customRender as render };
--- jest.config.js
+++ jest.config.js
@@ -26,6 +26,7 @@ const customJestConfig = {
     '^@pages/(.*)$': '<rootDir>/src/pages/$1',
     '^@services/(.*)$': '<rootDir>/src/services/$1',
     '^@styles/(.*)$': '<rootDir>/src/styles/$1',
+    '^@test-utils': '<rootDir>/__tests__/utils/test-utils',
     '^@ts/(.*)$': '<rootDir>/src/ts/$1',
     '^@utils/(.*)$': '<rootDir>/src/utils/$1',
   },
@@ -35,6 +36,11 @@ const customJestConfig = {
 
   // The test environment that will be used for testing
   testEnvironment: 'jest-environment-jsdom',
+
+  testPathIgnorePatterns: [
+    '<rootDir>/__tests__/jest/__mocks__',
+    '<rootDir>/__tests__/utils',
+  ],
 };
 
 /**
