2385ec8ca0963306f6b87564467301cb5a2a1834
Armand Philippot <git@armandphilippot.com>
2022-04-01 17:10:49 +0200

    build(storybook): handle SVGR import

--- .storybook/main.js
+++ .storybook/main.js
@@ -13,6 +13,16 @@ const storybookConfig = {
     builder: 'webpack5',
   },
   webpackFinal: async (config) => {
+    // Use SVGR for SVG files. See: https://medium.com/@derek_19900/config-storybook-4-to-use-svgr-webpack-plugin-22cb1152f004
+    const rules = config.module.rules;
+    const fileLoaderRule = rules.find((rule) => rule.test.test('.svg'));
+    fileLoaderRule.exclude = /\.svg$/;
+    rules.push({
+      test: /\.svg$/,
+      use: [{ loader: '@svgr/webpack', options: { dimensions: false } }],
+    });
+
+    // Set modules aliases.
     config.resolve.alias = {
       ...config.resolve.alias,
       '@i18n': path.resolve(__dirname, '../src/i18n'),
