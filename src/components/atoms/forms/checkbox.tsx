af575907c254d3233c3fd5904afc87c1db0bdcf3
Armand Philippot <git@armandphilippot.com>
2022-04-09 19:14:03 +0200

    chore: add a Checkbox component

---
+++ src/components/atoms/forms/checkbox.tsx
@@ -0,0 +1,46 @@
+import { SetStateAction, VFC } from 'react';
+
+export type CheckboxProps = {
+  /**
+   * One or more ids that refers to the checkbox name.
+   */
+  'aria-labelledby'?: string;
+  /**
+   * Add classnames to the checkbox.
+   */
+  className?: string;
+  /**
+   * Checkbox id attribute.
+   */
+  id: string;
+  /**
+   * Checkbox name attribute.
+   */
+  name: string;
+  /**
+   * Callback function to set checkbox value.
+   */
+  setValue: (value: SetStateAction<boolean>) => void;
+  /**
+   * Checkbox value.
+   */
+  value: boolean;
+};
+
+/**
+ * Checkbox component
+ *
+ * Render a checkbox type input.
+ */
+const Checkbox: VFC<CheckboxProps> = ({ value, setValue, ...props }) => {
+  return (
+    <input
+      type="checkbox"
+      checked={value}
+      onChange={() => setValue(!value)}
+      {...props}
+    />
+  );
+};
+
+export default Checkbox;
