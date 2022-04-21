34502bd004c2522a8f2a217da3adf51586d1dec3
Armand Philippot <git@armandphilippot.com>
2022-04-21 17:03:45 +0200

    chore: add a PageHeader component

---
+++ src/components/molecules/layout/page-header.test.tsx
@@ -0,0 +1,18 @@
+import { render, screen } from '@test-utils';
+import PageHeader from './page-header';
+
+const title = 'Non nemo amet';
+const intro =
+  'Suscipit omnis minima doloribus commodi. Laudantium similique ut enim voluptatem soluta maxime autem et.';
+
+describe('PageHeader', () => {
+  it('renders a title', () => {
+    render(<PageHeader title={title} intro={intro} />);
+    expect(screen.getByRole('heading', { level: 1 })).toHaveTextContent(title);
+  });
+
+  it('renders an introduction', () => {
+    render(<PageHeader title={title} intro={intro} />);
+    expect(screen.getByText(intro)).toBeInTheDocument();
+  });
+});
---
+++ src/components/molecules/layout/page-header.tsx
@@ -0,0 +1,49 @@
+import Heading from '@components/atoms/headings/heading';
+import styles from './page-header.module.scss';
+import Meta, { type MetaMap } from './meta';
+import { FC } from 'react';
+
+export type PageHeaderProps = {
+  /**
+   * Set additional classnames to the header element.
+   */
+  className?: string;
+  /**
+   * The page introduction.
+   */
+  intro?: string;
+  /**
+   * The page metadata.
+   */
+  meta?: MetaMap;
+  /**
+   * The page title.
+   */
+  title: string;
+};
+
+/**
+ * PageHeader component
+ *
+ * Render a header element with page title, meta and intro.
+ */
+const PageHeader: FC<PageHeaderProps> = ({
+  className = '',
+  intro,
+  meta,
+  title,
+}) => {
+  return (
+    <header className={`${styles.wrapper} ${className}`}>
+      <div className={styles.body}>
+        <Heading level={1} className={styles.title} withMargin={false}>
+          {title}
+        </Heading>
+        {meta && <Meta data={meta} className={styles.meta} layout="inline" />}
+        {intro && <div>{intro}</div>}
+      </div>
+    </header>
+  );
+};
+
+export default PageHeader;
