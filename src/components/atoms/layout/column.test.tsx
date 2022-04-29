2652fe4d5ee4451a0c9b52996844d90c66dfdfe0
Armand Philippot <git@armandphilippot.com>
2022-04-29 18:35:41 +0200

    chore: add a Column component

---
+++ src/components/atoms/layout/column.test.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from '@test-utils';
+import Column from './column';
+
+const body =
+  'Non praesentium voluptas quisquam ex est. Distinctio accusamus facilis libero in aut. Et veritatis quo impedit fugit amet sit accusantium. Ut est rerum asperiores sint libero eveniet. Molestias placeat recusandae suscipit eligendi sunt hic.';
+
+describe('Column', () => {
+  it('renders the column body', () => {
+    render(<Column>{body}</Column>);
+    expect(screen.getByText(body)).toBeInTheDocument();
+  });
+});
